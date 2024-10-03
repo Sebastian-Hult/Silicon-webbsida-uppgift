97c45f97027bb011a54826fa823c332755f49409
Sebastian Hult <sebastian.hult@utb.ecutbildning.se>
2024-10-03 18:23:01 +0200

    Elfte Commiten: Här jag jag gjort sectionen FAQ där jag har lagt in lite beskrivande text, contact us information samt en fungerande accordion. Medföljande styling i CSS såklart samt ett nyskapat JavaScript document som jag lagt i en JavaScript map. Detta JS dokument är det som styr min Accordion.

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,20 @@
+
+/* Taget från W3 schools HOW TO Creat An Accordion */
+
+/* Toggle för att ta fram eller ta bort klassen .active samt visa eller ta bort .panel */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
